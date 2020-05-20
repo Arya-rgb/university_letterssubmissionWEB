121e1e252a92e991a4f4c30a665e676756e0c216
Arya-rgb <dickyarya.personal@gmail.com>
2020-05-20 16:13:09 +0700

    initial commit. Remastered Status Surat With PlaceHolder

--- application/views/status_surat.php
+++ application/views/status_surat.php
@@ -1,6 +1,10 @@
 <html>
 <head>
-	<title>Status Surat</title>
+    <title>Status Surat</title>
+    <style type="text/css">
+	input{background: rgba(ffffff);}
+	input{border:0px; width: 100%;}
+    </style>
 </head>
 	<body>
 <table style="margin:20px auto;" border="1">
@@ -25,7 +29,7 @@ foreach($ajuan_surat as $a) {
 		<td><?php echo $a->email?></td>
 		<td><?php echo $a->kat_surat?></td>
 		<td><?php echo $a->jenis_surat?></td>
-		<td><a>Belum Tersedia</a><?php echo $a->status_surat?></td>
+		<td><input value="<?php echo $a->status_surat?>" placeholder="Belum Tersedia" readonly/></td>
 		<td><a href='https://<?php echo $a->link_surat?>'>Download</a></td>		
 	</tr>
 <?php } ?>
